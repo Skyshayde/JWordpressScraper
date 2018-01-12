e277ae4b5dfcff9103c7d38653a5d1ebd61edaf0
Teddy Heinen <skyshayde@gmail.com>
2018-01-12 15:34:31 -0600

    Update Readme

--- src/test/java/EpubTest.java
+++ src/test/java/EpubTest.java
@@ -13,8 +13,8 @@ class EpubTest {
         // A wordpress serial I read, using it to test.
 //        EpubLoader e = new EpubLoader("A Practical Guide to Evil - Erraticerrata.epub");
 //        return;
-        WordpressScraper scraper = new WordpressScraper("https://practicalguidetoevil.wordpress.com/2015/03/25/prologue/");
-        scraper.scrape();
-        new EpubBuilder(scraper.blog);
+        WordpressScraper scraper = new WordpressScraper("https://practicalguidetoevil.wordpress.com/2015/03/25/prologue/").scrape();
+        // ${title} and ${author} will be replaced by the actual title and author of the blog.
+        new EpubBuilder(scraper.blog, "${title} - ${author}");
     }
 }
